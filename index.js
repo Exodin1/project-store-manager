770708954caaea32440527fff0380b0608f81b27
Exodin1 <exodin1@outlook.com>
2022-01-25 15:31:26 -0300

    Configurando conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 
+const app = express();
+const bodyParser = require('body-parser');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
