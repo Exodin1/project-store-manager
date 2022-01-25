7ad7df019972121b0394f39eed9abef6b5aee092
Exodin1 <exodin1@outlook.com>
2022-01-25 15:04:16 -0300

    Configurando o index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.listen(process.env.PORT, () => {
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
